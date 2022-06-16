861a78fba737ab992e8f194d40128a7b80a24b26
alessandrolongo99 <longoalessandro153@gmail.com>
2022-06-16 16:02:46 +0200

    aggiunto append

--- js/main.js
+++ js/main.js
@@ -3,22 +3,34 @@ const row = document.querySelector('div.row');
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         console.log(i + ' fizzbuzz');
-        const element = '<div class="col bg-dark text-white box">fizzbuzz</div>';
-        row.innerHTML += element;
+        
+        const element = document.createElement('div');
+        element.className = 'col bg-danger ms_box';
+        element.append('fizzbuzz')
+        row.append(element);
 
     } else if (i % 3 == 0) {
         console.log(i + ' fizz');
-        const element = '<div class="col bg-dark text-white box">fizz</div>';
-        row.innerHTML += element;
+        
+        const element = document.createElement('div');
+        element.className = 'col bg-success ms_box';
+        element.append('fizz')
+        row.append(element);
 
     } else if (i % 5 == 0) {
         console.log(i + ' buzz');
-        const element = '<div class="col bg-dark text-white box">buzz</div>';
-        row.innerHTML += element;
+        
+        const element = document.createElement('div');
+        element.className = 'col bg-warning ms_box';
+        element.append('buzz')
+        row.append(element);
 
     } else {
         console.log(i);
-        const element = `<div class="col bg-dark text-white box">${i}</div>`;
-        row.innerHTML += element;
+        
+        const element = document.createElement('div');
+        element.className = 'col bg-primary ms_box';
+        element.append(i)
+        row.append(element);
     }
 }
